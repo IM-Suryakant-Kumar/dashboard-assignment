1ccb78b0498aac317310a206b066d6a8e14180e9
IM-Suryakant-Kumar <sujeetsaxena78@gmail.com>
2023-12-17 17:18:57 +0530

    feat: added Layout component

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,10 +1,26 @@
-function App() {
+import {
+	RouterProvider,
+	createBrowserRouter,
+	createRoutesFromElements,
+	Route,
+} from "react-router-dom";
+import Layout from "./components/Layout";
 
-  return (
-    <>
-      App
-    </>
-  )
+function App() {
+	const router = createBrowserRouter(
+		createRoutesFromElements(
+			<Route
+				path="/"
+				element={<Layout />}>
+				<Route
+					path="*"
+					element={<h1>404 - PAGE NOT FOUND</h1>}
+				/>
+			</Route>,
+		),
+	);
+
+	return <RouterProvider router={router} />;
 }
 
-export default App
+export default App;
---
+++ client/src/components/Layout.tsx
@@ -0,0 +1,12 @@
+import { Outlet } from "react-router"
+
+const Layout = () => {
+  return (
+    <div>
+        layout
+        <Outlet />
+    </div>
+  )
+}
+
+export default Layout

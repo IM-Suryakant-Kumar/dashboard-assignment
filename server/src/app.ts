bff7e13ee327e98063dc8ceed53712eacf3f4a63
IM-Suryakant-Kumar <sujeetsaxena78@gmail.com>
2023-12-17 12:27:09 +0530

    feat: added error classes

--- server/src/app.ts
+++ server/src/app.ts
@@ -1,27 +1,32 @@
 import "express-async-errors";
 import { config } from "dotenv";
+import morgan from "morgan";
+import cors from "cors";
 import express from "express";
 import notFoundMiddleware from "./middlewares/not-found";
 import errorHanlderMiddleware from "./middlewares/error-handler";
 
-config()
+config();
 
-const app = express()
+const app = express();
 
-app.use(express.json())
-app.use(express.urlencoded({ extended: true }))
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(morgan("tiny"));
+app.use(cors());
 
+const PORT: number = parseInt(process.env.PORT, 10) || 4000;
 
-const PORT: number = parseInt(process.env.PORT, 10) || 4000
-
-app.use(notFoundMiddleware)
-app.use(errorHanlderMiddleware)
+app.use(notFoundMiddleware);
+app.use(errorHanlderMiddleware);
 const start = async () => {
-    try {
-        app.listen(PORT, () => console.log(`Server is listen on port ${PORT}...`))
-    } catch (error) {
-        console.log(error)
-    }
-}
+	try {
+		app.listen(PORT, () =>
+			console.log(`Server is listen on port ${PORT}...`),
+		);
+	} catch (error) {
+		console.log(error);
+	}
+};
 
-start()
+start();
